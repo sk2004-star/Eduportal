3e5562a385b286119ce6c8e12e2b38c8ff81c00a
sk2004-sk <skimamuddin640@gmail.com>
2026-06-24 22:56:41 +0530

    Updated dashboard design

--- js/profile.js
+++ js/profile.js
@@ -13,66 +13,45 @@ from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
 
 onAuthStateChanged(auth, async(user)=>{
 
-```
-if(user){
+    if(user){
 
-    const docRef =
-    doc(db,"users",user.uid);
+        const docRef =
+        doc(db,"users",user.uid);
 
-    const docSnap =
-    await getDoc(docRef);
+        const docSnap =
+        await getDoc(docRef);
 
-    if(docSnap.exists()){
+        if(docSnap.exists()){
 
-        const data =
-        docSnap.data();
+            const data =
+            docSnap.data();
 
-        const displayName =
-        data.name || data.email;
+            const userEmail =
+            document.getElementById("userEmail");
 
-        const userEmail =
-        document.getElementById("userEmail");
+            const userRole =
+            document.getElementById("userRole");
 
-        const userEmailTop =
-        document.getElementById("userEmailTop");
+            const teacherEmail =
+            document.getElementById("teacherEmail");
 
-        const userRole =
-        document.getElementById("userRole");
+            if(userEmail){
+                userEmail.textContent =
+                data.name || data.email;
+            }
 
-        const teacherEmail =
-        document.getElementById("teacherEmail");
+            if(userRole){
+                userRole.textContent =
+                data.role;
+            }
 
-        const teacherEmailTop =
-        document.getElementById("teacherEmailTop");
+            if(teacherEmail){
+                teacherEmail.textContent =
+                data.name || data.email;
+            }
 
-        if(userEmail){
-            userEmail.textContent =
-            displayName;
-        }
-
-        if(userEmailTop){
-            userEmailTop.textContent =
-            displayName;
-        }
-
-        if(userRole){
-            userRole.textContent =
-            data.role;
-        }
-
-        if(teacherEmail){
-            teacherEmail.textContent =
-            displayName;
-        }
-
-        if(teacherEmailTop){
-            teacherEmailTop.textContent =
-            displayName;
         }
 
     }
-
-}
-```
 
 });
